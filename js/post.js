51677beb30da66cee231941bcdc7cf762af5632c
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-03-26 23:08:01 +0530

    half of edit function
    last commit of the day

--- js/post.js
+++ js/post.js
@@ -1,16 +1,23 @@
-function addComment(){
-    document.getElementById("comments").style.opacity="100%";
+function edit() {
+    var titletxt = document.getElementById("title").value;
+    var contenttxt = document.getElementById("content").value;
 
-    var text= document.getElementById("commentText").value;
-
-    var div=document.createElement("div");
-    div.id="comment";
-    div.innerText=text;
-
-    document.getElementById("comments").appendChild(div);
+    document.getElementById("titleDiv").innerHTML= '<textarea row="1" ';
 }
 
-function like(){
-    document.getElementById("likeBtn").innerHTML=(`<i class="fas fa-thumbs-up"></i> Liked !`);
-    document.getElementById("firstOne").innerText="1 person likes this!"
+function like() {
+    document.getElementById("likeBtn").innerHTML = (`<i class="fas fa-thumbs-up"></i> Liked !`);
+    document.getElementById("firstOne").innerText = "1 person likes this!"
+}
+
+function addComment() {
+    document.getElementById("comments").style.opacity = "100%";
+
+    var text = document.getElementById("commentText").value;
+
+    var div = document.createElement("div");
+    div.id = "comment";
+    div.innerText = text;
+
+    document.getElementById("comments").appendChild(div);
 }

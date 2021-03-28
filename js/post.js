de650de834e3921865b694aa79b8a0339a66bc60
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-03-28 21:14:29 +0530

    part 3 comments and like counter added
    Project finished

--- js/post.js
+++ js/post.js
@@ -1,12 +1,20 @@
+// variable to count the amount of likes
+var likes=0;
+
 function edit() {
     var titleTxt = document.getElementById("title").innerHTML;
     var contentTxt = document.getElementById("content").innerHTML;
 
+    /* this code sends a premade tag to replace the title with an input area with the title text already in it
+       and the css in the onkeypress is just to dynamically increase its width according to the length of text 
+    */
     document.getElementById("titleDiv").innerHTML = `<input id="title" type="text" value="${titleTxt}" onkeypress="this.style.width = ((this.value.length + 9) * 9) + 'px';">`;
 
+    // similar code that sends a tag with content which is added in the next line because sending it like title didnt work somehow 
     document.getElementById("contentDiv").innerHTML = `<textarea id="content" rows="13-" style="width:100%;"></textarea>`;
     document.getElementById("content").value=contentTxt;
 
+    // changing the onclick function and editing the contents of the buttton
     document.getElementById("edit").setAttribute('onclick','save()');
     document.getElementById("edit").innerHTML = `Save <i class="far fa-save"></i>`;
 }
@@ -15,6 +23,7 @@ function save(){
     var titleTxt = document.getElementById("title").value;
     var contentTxt = document.getElementById("content").value;
 
+    // sending tags with the updated value
     document.getElementById("titleDiv").innerHTML = `<h3 id="title"> ${titleTxt}</h3>`;
     document.getElementById("contentDiv").innerHTML = `<div id="content">${contentTxt}</div>`;
 
@@ -23,13 +32,18 @@ function save(){
 }
 
 function like() {
+    // basic counter code for the number of likes
     document.getElementById("likeBtn").innerHTML = (`<i class="fas fa-thumbs-up"></i> Liked!`);
-    document.getElementById("firstOne").innerText = "1 person likes this!"
+    likes=likes+1;
+    document.getElementById("firstOne").innerText = `${likes} people have liked this!`;
 }
 
 function addComment() {
     var text = document.getElementById("commentText").value;
+
+    //check for empty comment
     if(text){
+        // makes a new comment div and adds it
         var div = document.createElement("div");
         div.id = "comment";
         div.innerText = text;
